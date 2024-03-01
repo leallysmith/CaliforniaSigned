aa12fb1af851104eacbb1eee92eb72639d183552
leallysmith <89225401+leallysmith@users.noreply.github.com>
2024-03-01 07:40:31 -0700

    Revert "delete this"
    This reverts commit d3cea3ba1d92cb91275168c4eb95fbffdc9d576f.

--- styles/SignedParcelNameslastupdated21224_17_style.js
+++ styles/SignedParcelNameslastupdated21224_17_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_SignedParcelNames = function(feature, resolution){
+var style_SignedParcelNameslastupdated21224_17 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
